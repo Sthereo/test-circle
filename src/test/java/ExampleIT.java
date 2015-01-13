3a9ab45a0ddb311bbfc11987a62aab857550699c
Andres Sanz <asanzrivas@atlassian.com>
2015-01-13 20:55:00 +1100

    First commit with dummy unit and integration test
    * Use 'mvn integration-test -Pittest' to execute both unit and integration test

---
+++ src/test/java/ExampleIT.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Copyright Atlassian: 13/01/2015
+ */
+public class ExampleIT {
+    @Test
+    public void integrationTest1() throws Exception {
+        assertTrue(true);
+
+    }
+}
---
+++ src/test/java/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Copyright Atlassian: 13/01/2015
+ */
+public class ExampleUnitTest {
+    @org.junit.Test
+    public void unitTest1() throws Exception {
+        assertTrue(true);
+    }
+}
