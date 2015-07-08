1b5a897631ae1eb04ed335a801b7302602c2f161
Esther Asenjo <easenjo@atlassian.com>
2015-07-08 13:36:09 +1000

    Fixing more things

---
+++ src/test/java/ExampleUnit2Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Copyright Atlassian: 13/01/2015
+ */
+public class ExampleUnit2Test {
+    @org.junit.Test
+    public void unitTest2() throws Exception {
+        assertTrue(false);
+    }
+}
